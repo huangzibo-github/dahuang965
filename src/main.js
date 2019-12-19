799808f8aba70d4183af6dc81b63f28fc40f1f88
xiaohuang <965650203@qq.com>
2019-12-19 18:55:34 +0800

    191219黑马头条PC-登录模块-安装axios-配置全局使用

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'
+import axios from 'axios'
 import 'element-ui/lib/theme-chalk/index.css'
 import './styles/index.less'
 Vue.config.productionTip = false
-
 Vue.use(ElementUI)
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
+Vue.prototype.$http = axios
 new Vue({
   router,
   render: h => h(App)
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,19 +4,19 @@
       <div class="title">
         <img src="../../assets/img/logo_index.png" alt />
       </div>
-      <el-form style="margin-top:30px">
-        <el-form-item>
-          <el-input placeholder="请输入手机号"></el-input>
+      <el-form style="margin-top:30px" :model="LoginForm" :rules="loginRules" ref="myForm">
+        <el-form-item prop="mobile">
+          <el-input placeholder="请输入手机号" v-model="LoginForm.mobile"></el-input>
         </el-form-item>
-        <el-form-item>
-          <el-input style="width:60%" placeholder="请输入验证码"></el-input>
+        <el-form-item prop="code">
+          <el-input style="width:60%" placeholder="请输入验证码" v-model="LoginForm.code"></el-input>
           <el-button style="float:right" plain>发送验证码</el-button>
         </el-form-item>
-        <el-form-item>
-          <el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
+        <el-form-item prop="check">
+          <el-checkbox v-model="LoginForm.check">我已阅读并同意用户协议和隐私条款</el-checkbox>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" style="width:100%">登录</el-button>
+          <el-button @click="submitLogin" type="primary" style="width:100%">登录</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -24,7 +24,52 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      LoginForm: {
+        mobile: '', // 手机号
+        code: '', // 验证码
+        check: false // 勾选
+      },
+      loginRules: {
+        // 验证规则 验证登录表单的     必须有required true
+        mobile: [{
+          required: true,
+          message: '请输入手机号'
+        }, {
+          pattern: /^1[3456789]\d{9}$/,
+          message: '请输入合法的手机号'
+        }],
+        code: [{
+          required: true,
+          message: '请输入验证码'
+        }, {
+          pattern: /^\d{6}$/,
+          message: '请输入合法的验证码'
+        }],
+        check: [{ validator: function (rule, value, callback) {
+          // rule当前的规则，没什么用
+          // value指的是要校验的字段的值
+          if (value) {
+            callback()
+          } else {
+            callback(new Error('你是用户，所以你必须选择被坑！！'))
+          }
+        } }]
+      }
+    }
+  },
+  methods: {
+    submitLogin () {
+      this.$refs.myForm.validate(function (isOK) {
+        if (isOK) {
+          console.log('检验成功')
+        }
+      })
+    }
+  }
+}
 </script>
 <style lang="less" scoped>
 .login {
